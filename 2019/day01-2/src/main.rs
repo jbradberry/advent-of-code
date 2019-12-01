e5b272124461387fc5a9324c9109da952aeff617
Jeff Bradberry <jeff.bradberry@gmail.com>
2019-12-01 00:28:28 -0500

    Solution for day01 part 2

--- 2019/day01-2/src/main.rs
+++ 2019/day01-2/src/main.rs
@@ -8,12 +8,30 @@ fn read() -> Vec<u32> {
 }
 
 
-fn fuel_req(mass: u32) -> u32 { mass / 3 - 2 }
+fn fuel_req(mass: u32) -> u32 {
+    let third = mass / 3;
+    if third >= 2 { third - 2 }
+    else { 0 }
+}
+
+
+fn rocket_fuel_req(mass: u32) -> u32 {
+    let mut total_fuel = 0;
+    let mut fuel = mass;
+
+    loop {
+        fuel = fuel_req(fuel);
+        if fuel > 0 { total_fuel += fuel; }
+        else { break }
+    }
+
+    total_fuel
+}
 
 
 fn main() {
     let mass = read();
-    let total: u32 = mass.iter().map(|&m| fuel_req(m)).sum();
+    let total: u32 = mass.iter().map(|&m| rocket_fuel_req(m)).sum();
 
     println!("total fuel requirement: {}", total);
 }
