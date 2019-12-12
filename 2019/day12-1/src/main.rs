763fe69667efc13d14320369c0632276455bc533
Jeff Bradberry <jeff.bradberry@gmail.com>
2019-12-12 00:30:38 -0500

    Broken algorithm

--- 2019/day12-1/src/main.rs
+++ 2019/day12-1/src/main.rs
@@ -1,14 +1,19 @@
+use std::cmp::Ordering;
 use std::io;
 use std::io::prelude::*;
 
+use itertools::Itertools;
 use regex::Regex;
 
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Moon {
     x: i32,
     y: i32,
     z: i32,
+    vx: i32,
+    vy: i32,
+    vz: i32,
 }
 
 
@@ -23,13 +28,41 @@ fn read() -> Vec<Moon> {
             x: cap.get(1).unwrap().as_str().parse().unwrap(),
             y: cap.get(2).unwrap().as_str().parse().unwrap(),
             z: cap.get(3).unwrap().as_str().parse().unwrap(),
+            vx: 0, vy: 0, vz: 0,
         }
     }).collect()
 }
 
 
 fn main() {
-    let moons = read();
+    let mut moons = read();
 
-    println!("{:?}", moons);
+    for step in 0..1000 {
+        for pair in (0..4).combinations(2) {
+            let m1 = &moons[pair[0]];
+            let m2 = &moons[pair[1]];
+
+            match m1.x.cmp(&m2.x) {
+                Ordering::Less => { m1.vx += 1; m2.vx -= 1; },
+                Ordering::Greater => { m1.vx -= 1; m2.vx += 1; },
+                Ordering::Equal => {},
+            }
+            match m1.y.cmp(&m2.y) {
+                Ordering::Less => { m1.vy += 1; m2.vy -= 1; },
+                Ordering::Greater => { m1.vy -= 1; m2.vy += 1; },
+                Ordering::Equal => {},
+            }
+            match m1.z.cmp(&m2.z) {
+                Ordering::Less => { m1.vz += 1; m2.vz -= 1; },
+                Ordering::Greater => { m1.vz -= 1; m2.vz += 1; },
+                Ordering::Equal => {},
+            }
+        }
+
+        for moon in &moons {
+            moon.x += moon.vx;
+            moon.y += moon.vy;
+            moon.z += moon.vz;
+        }
+    }
 }
