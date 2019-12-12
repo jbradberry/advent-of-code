f821934069d993294314ae1f5c24985011474601
Jeff Bradberry <jeff.bradberry@gmail.com>
2019-12-12 10:16:56 -0500

    Naive algorithm for part 2
    This triggers the OOM killer.

---
+++ 2019/day12-2/src/main.rs
@@ -0,0 +1,82 @@
+use std::cmp::Ordering;
+use std::collections::HashSet;
+use std::io;
+use std::io::prelude::*;
+
+use itertools::Itertools;
+use regex::Regex;
+
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct Moon {
+    x: i32,
+    y: i32,
+    z: i32,
+    vx: i32,
+    vy: i32,
+    vz: i32,
+}
+
+
+fn read() -> Vec<Moon> {
+    let re = Regex::new(r"^<x=([-0-9]+),[ ]*y=([-0-9]+),[ ]*z=([-0-9]+)>$").unwrap();
+    let stdin = io::stdin();
+    stdin.lock().lines().map(|x| {
+        let line = x.unwrap();
+        let cap = re.captures(&line).unwrap();
+
+        Moon {
+            x: cap.get(1).unwrap().as_str().parse().unwrap(),
+            y: cap.get(2).unwrap().as_str().parse().unwrap(),
+            z: cap.get(3).unwrap().as_str().parse().unwrap(),
+            vx: 0, vy: 0, vz: 0,
+        }
+    }).collect()
+}
+
+
+fn main() {
+    let mut moons = read();
+    let mut previous = HashSet::new();
+    previous.insert((moons[0], moons[1], moons[2], moons[3]));
+
+    let mut step = 0;
+    loop {
+        for pair in (0..4).combinations(2) {
+            let mut m1 = moons[pair[0]];
+            let mut m2 = moons[pair[1]];
+
+            match m1.x.cmp(&m2.x) {
+                Ordering::Less => { m1.vx += 1; m2.vx -= 1; },
+                Ordering::Greater => { m1.vx -= 1; m2.vx += 1; },
+                Ordering::Equal => {},
+            }
+            match m1.y.cmp(&m2.y) {
+                Ordering::Less => { m1.vy += 1; m2.vy -= 1; },
+                Ordering::Greater => { m1.vy -= 1; m2.vy += 1; },
+                Ordering::Equal => {},
+            }
+            match m1.z.cmp(&m2.z) {
+                Ordering::Less => { m1.vz += 1; m2.vz -= 1; },
+                Ordering::Greater => { m1.vz -= 1; m2.vz += 1; },
+                Ordering::Equal => {},
+            }
+
+            moons[pair[0]] = m1;
+            moons[pair[1]] = m2;
+        }
+
+        for moon in &mut moons {
+            moon.x += moon.vx;
+            moon.y += moon.vy;
+            moon.z += moon.vz;
+        }
+
+        let key = (moons[0], moons[1], moons[2], moons[3]);
+        if previous.contains(&key) { break; }
+        previous.insert(key);
+        step += 1;
+    }
+
+    println!("steps to repeat: {}", step);
+}
