9ac0ce89da0ed0e51eeb4f7bfb68a8618e56d6b6
Jeff Bradberry <jeff.bradberry@gmail.com>
2019-12-14 02:16:48 -0500

    Attempt at writing a Gaussian Elimination algorithm
    This isn't really correct, since we need to work with integer
    quantities, even if there winds up being wasted resources.

--- 2019/day14-1/src/main.rs
+++ 2019/day14-1/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::io;
 use std::io::prelude::*;
 
@@ -31,4 +31,39 @@ fn read() -> Vec<HashMap<String, i32>>{
 
 fn main() {
     let mut data = read();
+    let vars = data.iter().map(|r| r.keys())
+        .flatten()
+        .cloned()
+        .filter(|x| x != "FUEL" && x != "ORE")
+        .collect::<HashSet<String>>();
+    let mut columns = vars.into_iter().collect::<Vec<_>>();
+    columns.insert(0, "FUEL".to_string());
+
+    assert_eq!(columns.len(), data.len());
+
+    columns.push("ORE".to_string());
+
+    let mut matrix = data.iter()
+        .map(|r| columns.iter().cloned().map(|c| *r.get(&c).unwrap_or(&0) as f64).collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+    let n = data.len();
+    for i in 1..=(n - 1) {
+        let p = (i..=n).filter(|x| matrix[x - 1][i - 1] != 0.0).min().expect("No unique solution exists.");
+        if p != i { matrix.swap(p - 1, i - 1); }
+        for j in (i + 1)..=n {
+            let m = matrix[j - 1][i - 1] / matrix[i - 1][i - 1];
+            for x in 1..=(n + 1) {
+                matrix[j - 1][x - 1] = matrix[j - 1][x - 1] - m * matrix[i - 1][x - 1];
+            }
+        }
+    }
+    if matrix[n - 1][n - 1] == 0.0 { panic!("No unique solution exists."); }
+    let mut x = vec![0.0; n];
+    x[n - 1] = matrix[n - 1][n] / matrix[n - 1][n - 1];
+    for i in 1..n {
+        x[n - i - 1] = (matrix[i - 1][n] - ((n - i)..=n).map(|p| matrix[n - i - 1][p - 1] * x[p - 1]).sum::<f64>())
+            / matrix[n - i - 1][n - i - 1];
+    }
+
+    println!("{:?}", x);
 }
