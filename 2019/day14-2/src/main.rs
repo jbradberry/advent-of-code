9d09e6bb0415fef024e59b00b0056bd2f3bc7cdf
Jeff Bradberry <jeff.bradberry@gmail.com>
2019-12-14 18:52:18 -0500

    Let the computer do the binary search for me

--- 2019/day14-2/src/main.rs
+++ 2019/day14-2/src/main.rs
@@ -30,33 +30,25 @@ fn read() -> HashMap<String, (u64, HashMap<String, u64>)> {
 }
 
 
-fn main() {
-    let mut data = read();
+fn solve(data: &HashMap<String, (u64, HashMap<String, u64>)>, fuel: u64) -> u64 {
+    let mut data_copy = data.clone();
 
-    let fuel = 3_566_577;  // discovered through an ugly manual binary search on likely values,
-                           // replacing this value then recompiling :(
-    println!("fuel: {}", fuel);
-
-    let (_, mut inputs) = data.remove("FUEL").unwrap();
+    let (_, mut inputs) = data_copy.remove("FUEL").unwrap();
     for (_, value) in inputs.iter_mut() {
         *value *= fuel;
     }
 
     while inputs.len() > 1 {
-        println!("inputs: {:?}", inputs);
-
-        let counts = data.iter()
+        let counts = data_copy.iter()
             .map(|(_, (_, si))| si.keys())
             .flatten()
             .collect::<Counter<_>>();
-
-        println!("counts: {:?}", counts.most_common());
 
         let key = inputs.keys().cloned()
             .filter(|k| k != "ORE")
             .min_by_key(|k| counts.get(k).unwrap_or(&0)).unwrap();
         let value = inputs.remove(&key).unwrap();
-        let (quant, sub_inputs) = data.remove(&key).unwrap();
+        let (quant, sub_inputs) = data_copy.remove(&key).unwrap();
         let (mut multiplier, remainder) = (value / quant, value % quant);
         if remainder > 0 { multiplier += 1; }
 
@@ -65,5 +57,28 @@ fn main() {
         }
     }
 
-    println!("{:?}", inputs);
+    *inputs.get("ORE").unwrap()
+}
+
+
+fn main() {
+    let data = read();
+    let ore = solve(&data, 1);
+
+    println!("Part 1");
+    println!("ore: {}", ore);
+
+    let mut lower = 1_000_000_000_000 / ore;
+    assert!(solve(&data, lower) < 1_000_000_000_000);
+    let mut upper = 2 * lower;
+    assert!(solve(&data, upper) > 1_000_000_000_000);
+
+    let mut fuel = lower + (upper - lower) / 2;
+    while fuel > lower {
+        if solve(&data, fuel) <= 1_000_000_000_000 { lower = fuel; } else { upper = fuel; }
+        fuel = lower + (upper - lower) / 2;
+    }
+
+    println!("Part 2");
+    println!("1 trillion ORE => fuel: {}", fuel);
 }
