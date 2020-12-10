9b84cbc7cd8dac9b41595c880781534cfcc58b2f
Jeff Bradberry <jeff.bradberry@gmail.com>
2020-12-10 09:20:08 -0500

    Solve part 1

--- 2020/day10-1/src/main.rs
+++ 2020/day10-1/src/main.rs
@@ -7,15 +7,30 @@ use itertools::Itertools;
 fn read() -> Vec<u16> {
     let stdin = io::stdin();
 
-    stdin.lock().lines()
+    let mut data = vec![0];
+    let input = stdin.lock().lines()
         .map(|s| s.unwrap().parse().unwrap())
         .sorted()
-        .collect()
+        .collect::<Vec<u16>>();
+    let device = input.iter().max().unwrap() + 3;
+
+    data.extend(input);
+    data.push(device);
+
+    data
 }
 
 
 fn main() {
     let joltages = read();
 
-    println!("{:?}", joltages);
+    let diffs = joltages.iter()
+        .zip(joltages[1..].iter())
+        .map(|(a, b)| b - a)
+        .collect::<Vec<u16>>();
+
+    let ones = diffs.iter().filter(|&x| *x == 1).count();
+    let threes = diffs.iter().filter(|&x| *x == 3).count();
+
+    println!("{} * {} = {}", ones, threes, ones * threes);
 }
