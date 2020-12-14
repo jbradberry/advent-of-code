c267a61f96ff7143235650f09cdaa1b0c5de2b1e
Jeff Bradberry <jeff.bradberry@gmail.com>
2020-12-14 11:11:16 -0500

    Solve part 1

--- 2020/day14-1/src/main.rs
+++ 2020/day14-1/src/main.rs
@@ -8,8 +8,8 @@ use regex::Regex;
 
 #[derive(Debug, Clone)]
 enum Instruction {
-    Mask((u64, u64)),
-    Mem((usize, u64)),
+    Mask(u64, u64),
+    Mem(usize, u64),
 }
 
 
@@ -27,13 +27,13 @@ fn read() -> Vec<Instruction> {
                 let mask = &caps[1];
 
                 Instruction::Mask(
-                    (u64::from_str_radix(&mask.replace("1", "0").replace("X", "1"), 2).unwrap(),
-                     u64::from_str_radix(&mask.replace("X", "0"), 2).unwrap())
+                    u64::from_str_radix(&mask.replace("1", "0").replace("X", "1"), 2).unwrap(),
+                    u64::from_str_radix(&mask.replace("X", "0"), 2).unwrap()
                 )
             }
             else {
                 let caps = re_mem.captures(&line).unwrap();
-                Instruction::Mem((caps[1].parse().unwrap(), caps[2].parse().unwrap()))
+                Instruction::Mem(caps[1].parse().unwrap(), caps[2].parse().unwrap())
             }
         })
         .collect()
@@ -43,5 +43,16 @@ fn read() -> Vec<Instruction> {
 fn main() {
     let program = read();
 
-    println!("{:?}", program);
+    let mut memory = HashMap::new();
+    let mut disable = 0;
+    let mut force = 0;
+
+    for inst in program {
+        match inst {
+            Instruction::Mask(a, b) => { disable = a; force = b; },
+            Instruction::Mem(loc, val) => { memory.insert(loc, val & disable | force); },
+        }
+    }
+
+    println!("sum: {}", memory.values().sum::<u64>());
 }
