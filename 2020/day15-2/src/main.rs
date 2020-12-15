b6a184dea4c0a30db68809a7686357762028d7ff
Jeff Bradberry <jeff.bradberry@gmail.com>
2020-12-15 01:22:51 -0500

    Solve part 2

---
+++ 2020/day15-2/src/main.rs
@@ -0,0 +1,39 @@
+use std::io;
+use std::io::prelude::*;
+use std::collections::HashMap;
+
+
+fn read() -> Vec<u64> {
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer).unwrap();
+
+    buffer.trim().split(",").map(|s| s.parse().unwrap()).collect()
+}
+
+
+fn say(last_said: &mut HashMap<u64, usize>, num: u64, counter: usize) -> u64 {
+    // println!("say -- num: {}, counter: {}", num, counter);
+    let prev = *last_said.get(&num).unwrap_or(&counter);
+    last_said.insert(num, counter);
+
+    (counter - prev) as u64
+}
+
+
+fn main() {
+    let list = read();
+    let mut previous = list.iter().enumerate().map(|(i, n)| (*n, i + 1)).collect::<HashMap<_, _>>();
+
+    let mut last = *list.iter().last().unwrap();
+    let mut counter = list.len();
+    loop {
+        last = say(&mut previous, last, counter);
+        counter += 1;
+
+        // println!("turn: {}, number: {}", counter, last);
+
+        if counter >= 30_000_000 { break; }
+    }
+
+    println!("number 30,000,000: {}", last);
+}
