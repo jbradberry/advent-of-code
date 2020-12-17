d1134a3ea4e5474d64ca64978c4bceae6713981c
Jeff Bradberry <jeff.bradberry@gmail.com>
2020-12-17 11:14:53 -0500

    Add a function to display the grid

--- 2020/day17-1/src/main.rs
+++ 2020/day17-1/src/main.rs
@@ -22,8 +22,28 @@ fn read() -> HashMap<(i64, i64, i64), bool> {
 }
 
 
+fn render_grid(grid: &HashMap<(i64, i64, i64), bool>) {
+    let min_x = grid.keys().map(|(x, _, _)| *x).min().unwrap();
+    let max_x = grid.keys().map(|(x, _, _)| *x).max().unwrap();
+    let min_y = grid.keys().map(|(_, y, _)| *y).min().unwrap();
+    let max_y = grid.keys().map(|(_, y, _)| *y).max().unwrap();
+    let min_z = grid.keys().map(|(_, _, z)| *z).min().unwrap();
+    let max_z = grid.keys().map(|(_, _, z)| *z).max().unwrap();
+
+    for z in min_z..=max_z {
+        println!("z = {}", z);
+        for x in min_x..=max_x {
+            let row: String = (min_y..=max_y)
+                .map(|y| if *grid.get(&(x, y, z)).unwrap_or(&false) { '#' } else { '.' })
+                .collect();
+            println!("{}", row);
+        }
+    }
+}
+
+
 fn main() {
     let mut grid = read();
 
-    println!("{:?}", grid);
+    render_grid(&grid);
 }
