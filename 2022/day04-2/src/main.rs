82b7aba3b34868ea03c00e6b21469f5851a219f4
Jeff Bradberry <jeff.bradberry@gmail.com>
2022-12-04 13:41:54 -0500

    Count the number of assignments that overlap at all

---
+++ 2022/day04-2/src/main.rs
@@ -0,0 +1,42 @@
+use std::io;
+use std::io::prelude::*;
+
+use regex::Regex;
+
+
+fn read() -> Vec<(u16, u16, u16, u16)> {
+    let re = Regex::new(r"^(\d+)-(\d+),(\d+)-(\d+)$").unwrap();
+    let stdin = io::stdin();
+
+    stdin.lock().lines()
+        .map(|x| {
+            let line = x.unwrap();
+            let cap = re.captures(&line).unwrap();
+
+            (cap.get(1).unwrap().as_str().parse().unwrap(),
+             cap.get(2).unwrap().as_str().parse().unwrap(),
+             cap.get(3).unwrap().as_str().parse().unwrap(),
+             cap.get(4).unwrap().as_str().parse().unwrap())
+        })
+        .collect()
+}
+
+
+fn overlap(x: &(u16, u16, u16, u16)) -> bool {
+    if (x.0 <= x.2) && (x.1 >= x.2) { return true }
+    if (x.0 <= x.3) && (x.1 >= x.3) { return true }
+    if (x.2 <= x.0) && (x.3 >= x.0) { return true }
+    if (x.2 <= x.1) && (x.3 >= x.1) { return true }
+    false
+}
+
+
+fn main() {
+    let assignments = read();
+
+    let overlaps = assignments.into_iter()
+        .filter(overlap)
+        .count();
+
+    println!("overlaps: {}", overlaps);
+}
