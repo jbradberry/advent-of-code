8c8fd18e0970c85833c1baf78c67b8ed6fed776b
Jeff Bradberry <jeff.bradberry@gmail.com>
2022-12-04 11:43:57 -0500

    Calculate the score

--- 2022/day02-1/src/main.rs
+++ 2022/day02-1/src/main.rs
@@ -15,8 +15,35 @@ fn read() -> Vec<Vec<char>> {
 }
 
 
+fn score(strategy: &Vec<Vec<char>>) -> u32 {
+    strategy.iter()
+        .map(|r| {
+            let a = match r[1] {
+                'X' => 1,
+                'Y' => 2,
+                'Z' => 3,
+                _ => 0
+            };
+
+            let b = match (r[0], r[1]) {
+                ('A', 'Y') => 6,
+                ('B', 'Z') => 6,
+                ('C', 'X') => 6,
+                ('A', 'X') => 3,
+                ('B', 'Y') => 3,
+                ('C', 'Z') => 3,
+                _ => 0
+            };
+
+            a + b
+        })
+        .sum::<u32>()
+}
+
+
 fn main() {
     let strategy = read();
+    let score = score(&strategy);
 
-    println!("strategy: {:?}", strategy);
+    println!("score: {}", score);
 }
