39d472e1d3d655ddcb98f600c7c36e9cd6805b7e
Jeff Bradberry <jeff.bradberry@gmail.com>
2022-12-05 11:14:40 -0500

    Do the crane operations with crate ordering preserved

---
+++ 2022/day05-2/src/main.rs
@@ -0,0 +1,73 @@
+use std::io;
+use std::io::prelude::*;
+
+use regex::Regex;
+
+
+fn read() -> (Vec<Vec<char>>, Vec<(u16, u16, u16)>) {
+    let re = Regex::new(r"^move (\d+) from (\d+) to (\d+)$").unwrap();
+    let stdin = io::stdin();
+
+    let lines = stdin.lock().lines().map(|s| s.unwrap()).collect::<Vec<_>>();
+    let mut sections = lines.split(|s| s == "");
+
+    let mut diagram = sections.next().unwrap().iter().collect::<Vec<_>>();
+    diagram.reverse();
+
+    let mut stacks = diagram[0]
+        .split(" ")
+        .filter(|s| *s != "")
+        .map(|_| Vec::new())
+        .collect::<Vec<_>>();
+    for line in diagram {
+        for (i, c) in line.chars().enumerate() {
+            match c {
+                'A'..='Z' => { stacks[((i - 1) / 4) as usize].push(c); },
+                _ => { continue; }
+            }
+        }
+    }
+
+    let instructions = sections.next().unwrap()
+        .iter()
+        .map(|line| {
+            let cap = re.captures(&line).unwrap();
+
+            (cap.get(1).unwrap().as_str().parse().unwrap(),
+             cap.get(2).unwrap().as_str().parse().unwrap(),
+             cap.get(3).unwrap().as_str().parse().unwrap())
+        })
+        .collect();
+
+    (stacks, instructions)
+}
+
+
+fn action(stacks: &mut Vec<Vec<char>>, action: &(u16, u16, u16)) {
+    let mut queue = Vec::new();
+
+    for _ in 0..action.0 {
+        queue.push(stacks[(action.1 - 1) as usize].pop().unwrap());
+    }
+
+    for _ in 0..action.0 {
+        let c = queue.pop().unwrap();
+        stacks[(action.2 - 1) as usize].push(c);
+    }
+}
+
+
+fn main() {
+    let (mut stacks, instructions) = read();
+    println!("instructions: {:?}", instructions);
+    println!("stacks before: {:?}", stacks);
+
+    for inst in &instructions {
+        action(&mut stacks, inst);
+    }
+
+    println!("stacks after: {:?}", stacks);
+
+    let result = stacks.iter().map(|x| x.last().unwrap()).collect::<String>();
+    println!("result: {}", result);
+}
