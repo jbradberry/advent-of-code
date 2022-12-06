f53784fcee3e13214970d424b1dd050e2847eb87
Jeff Bradberry <jeff.bradberry@gmail.com>
2022-12-06 10:33:23 -0500

    Find the first index where the last 4 characters are different

--- 2022/day06-1/src/main.rs
+++ 2022/day06-1/src/main.rs
@@ -1,5 +1,19 @@
 use std::io;
 
+use itertools::Itertools;
+
+
+fn find_header(s: &str) -> usize {
+    s.chars()
+        .tuple_windows::<(_, _, _, _)>()
+        .enumerate()
+        .find_map(|(i, (a, b, c, d))| {
+            if (a == b) || (a == c) || (a == d) || (b == c) || (b == d) || (c == d) { return None }
+            Some(i + 4)
+        })
+        .unwrap()
+}
+
 
 fn main() {
     let mut datastream = String::new();
@@ -8,4 +22,8 @@ fn main() {
         .expect("Failure to read data stream.");
 
     println!("data stream: {:?}", datastream);
+
+    let index = find_header(&datastream);
+
+    println!("index of header: {}", index);
 }
