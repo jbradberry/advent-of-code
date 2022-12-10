d6d3b9e40317e2cc4eef7ae5ec82b6944bb71258
Jeff Bradberry <jeff.bradberry@gmail.com>
2022-12-10 10:07:05 -0500

    Pull out the results for the relevant CPU cycles

--- 2022/day10-1/src/main.rs
+++ 2022/day10-1/src/main.rs
@@ -35,5 +35,28 @@ fn read() -> Vec<OpCode> {
 
 fn main() {
     let instructions = read();
-    println!("instructions: {:#?}", instructions);
+    // println!("instructions: {:?}", instructions);
+
+    let mut current = 1;
+    let cycles = instructions.iter()
+        .flat_map(|inst| {
+            match inst {
+                OpCode::Noop => { vec![current] },
+                OpCode::Addx(x) => {
+                    let old = current;
+                    current += x;
+                    vec![old, old]
+                },
+            }
+        })
+        .collect::<Vec<_>>();
+
+    // println!("cycles: {:#?}", cycles);
+
+    let results = cycles.iter()
+        .enumerate()
+        .filter(|(i, _)| (i + 1) % 40 == 20)
+        .map(|(_, x)| x)
+        .collect::<Vec<_>>();
+    println!("results: {:?}", results);
 }
