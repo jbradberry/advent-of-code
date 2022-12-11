8e330ab75ce564719bbaa48d3494d30516475e7e
Jeff Bradberry <jeff.bradberry@gmail.com>
2022-12-11 10:53:27 -0500

    Track the worry level using the modulo against all of the monkey's tests

---
+++ 2022/day11-2/src/main.rs
@@ -0,0 +1,120 @@
+use std::io;
+use std::io::prelude::*;
+
+use regex::Regex;
+
+
+#[derive(Debug, Default)]
+struct Monkey {
+    items: Vec<u64>,
+    operation: (String, String, String),
+    test: u64,
+    test_true: usize,
+    test_false: usize,
+}
+
+
+fn read() -> Vec<Monkey> {
+    let re_start = Regex::new(r"Starting items: (\d+(?:, \d+)*)$").unwrap();
+    let re_operation = Regex::new(r"Operation: new = (old|\d+) ([+*]) (old|\d+)$").unwrap();
+    let re_test = Regex::new(r"Test: divisible by (\d+)$").unwrap();
+    let re_true = Regex::new(r"If true: throw to monkey (\d+)$").unwrap();
+    let re_false = Regex::new(r"If false: throw to monkey (\d+)$").unwrap();
+
+    let mut results = Vec::new();
+    let mut monkey: Monkey = Default::default();
+
+    let stdin = io::stdin();
+    for l in stdin.lock().lines() {
+        let line = l.unwrap();
+        if line == "" {
+            results.push(monkey);
+            monkey = Default::default();
+            continue;
+        }
+
+        if re_start.is_match(&line) {
+            monkey.items = re_start.captures(&line).unwrap()[1]
+                .split(", ")
+                .map(|x| x.parse().unwrap())
+                .collect();
+        }
+        if re_operation.is_match(&line) {
+            let cap = re_operation.captures(&line).unwrap();
+            monkey.operation = (cap[1].to_string(), cap[2].to_string(), cap[3].to_string());
+        }
+        if re_test.is_match(&line) {
+            monkey.test = re_test.captures(&line).unwrap()[1].parse().unwrap();
+        }
+        if re_true.is_match(&line) {
+            monkey.test_true = re_true.captures(&line).unwrap()[1].parse().unwrap();
+        }
+        if re_false.is_match(&line) {
+            monkey.test_false = re_false.captures(&line).unwrap()[1].parse().unwrap();
+        }
+    }
+    results.push(monkey);
+
+    results
+}
+
+
+fn main() {
+    let mut monkeys = read();
+
+    // println!("monkeys: {:#?}", monkeys);
+
+    // Since we are using modulo arithmetic for each test, we just
+    // need to ensure that each item's worry value maintains the same
+    // modulo value for each possible monkey.  Taking the product of
+    // all of the monkey's tests should do that.
+    let modulus = monkeys.iter().fold(1, |acc, m| acc * m.test);
+
+    let mut inspections = vec![0; monkeys.len()];
+    for _round in 0..10_000 {
+        for i in 0..monkeys.len() {
+            let mut throws = Vec::new();
+            {
+                let current_monkey = &monkeys[i];
+                for item in &current_monkey.items {
+                    let op = match current_monkey.operation.1.as_str() {
+                        "+" => u64::checked_add,
+                        "*" => u64::checked_mul,
+                        _ => unreachable!(),
+                    };
+                    let a = match current_monkey.operation.0.as_str() {
+                        "old" => *item,
+                        x @ _ => x.parse().unwrap(),
+                    };
+                    let b = match current_monkey.operation.2.as_str() {
+                        "old" => *item,
+                        x @ _ => x.parse().unwrap(),
+                    };
+
+                    let mut new_level = op(a, b).unwrap();
+                    new_level = new_level % modulus;
+
+                    let new_monkey = match new_level % current_monkey.test == 0 {
+                        true => current_monkey.test_true,
+                        false => current_monkey.test_false,
+                    };
+                    throws.push((new_monkey, new_level));
+                }
+            }
+            inspections[i] += monkeys[i].items.len();
+            monkeys[i].items.clear();
+            for (i2, item) in throws.into_iter() {
+                monkeys[i2].items.push(item);
+            }
+        }
+    }
+
+    for (i, insp) in inspections.iter().enumerate() {
+        println!("Monkey {} inspected items {} times.", i, insp);
+    }
+    println!("");
+    inspections.sort();
+    inspections.reverse();
+
+    println!("monkey business: {}", inspections[0] * inspections[1]);
+}
