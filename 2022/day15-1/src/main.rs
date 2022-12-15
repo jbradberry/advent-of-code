4b5b0ec8d421ef04962b6d9d4c4f3ee5e683d87d
Jeff Bradberry <jeff.bradberry@gmail.com>
2022-12-15 12:12:50 -0500

    Calculate the excluded spaces on line 2,000,000

--- 2022/day15-1/src/main.rs
+++ 2022/day15-1/src/main.rs
@@ -1,6 +1,8 @@
 use std::io;
 use std::io::prelude::*;
 
+use std::collections::HashSet;
+
 use regex::Regex;
 
 
@@ -22,6 +24,25 @@ fn read() -> Vec<((isize, isize), (isize, isize))> {
 
 fn main() {
     let sensor_info = read();
+    let test_line: isize = 2_000_000;
 
-    println!("info: {:#?}", sensor_info);
+    // println!("info: {:#?}", sensor_info);
+
+    let excluded = sensor_info.iter()
+        .map(|(x1, x2)| {
+            let d = (x1.0 - x2.0).abs() + (x1.1 - x2.1).abs();
+            (x1, d)
+        })
+        .filter(|(x1, d)| ((x1.1 - d)..=(x1.1 + d)).contains(&test_line))
+        .flat_map(|(x1, d)| {
+            let d2 = d - (x1.1 - test_line).abs();
+            (x1.0 - d2)..=(x1.0 + d2)
+        })
+        .collect::<HashSet<isize>>();
+    let beacons = sensor_info.iter()
+        .filter(|(_, x2)| x2.1 == test_line)
+        .map(|(_, x2)| x2.0)
+        .collect::<HashSet<isize>>();
+
+    println!("excluded at y={}: {}", test_line, excluded.difference(&beacons).collect::<Vec<_>>().len());
 }
