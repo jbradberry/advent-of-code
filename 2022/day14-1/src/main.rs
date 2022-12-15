efebb457c8407992b691d43877827a9ae5a6a005
Jeff Bradberry <jeff.bradberry@gmail.com>
2022-12-14 19:19:05 -0500

    Accumulate the grid and attempt to display it
    This is broken

--- 2022/day14-1/src/main.rs
+++ 2022/day14-1/src/main.rs
@@ -1,6 +1,29 @@
 use std::io;
 use std::io::prelude::*;
 
+use std::collections::HashMap;
+
+
+#[derive(Debug, Copy, Clone)]
+enum Tile {
+    Void,
+    Rock,
+    Sand,
+    Source,
+}
+
+
+impl From<char> for Tile {
+    fn from(c: char) -> Self {
+        match c {
+            '#' => Self::Rock,
+            'o' => Self::Sand,
+            '+' => Self::Source,
+            _ => Self::Void,
+        }
+    }
+}
+
 
 fn read() -> Vec<Vec<(usize, usize)>> {
     let stdin = io::stdin();
@@ -18,8 +41,41 @@ fn read() -> Vec<Vec<(usize, usize)>> {
 }
 
 
+fn calculate_grid(paths: &Vec<Vec<(usize, usize)>>) -> HashMap<(usize, usize), Tile> {
+    let mut grid: HashMap<(usize, usize), Tile> = paths.iter()
+        .flat_map(|p| p.iter().map(|&c| (c, Tile::Rock)))
+        .collect();
+
+    grid.insert((500, 0), Tile::Source);
+
+    grid
+}
+
+
+fn display(grid: &HashMap<(usize, usize), Tile>) {
+    let min_row = grid.keys().map(|&(r, _)| r).min().unwrap();
+    let max_row = grid.keys().map(|&(r, _)| r).max().unwrap();
+    let min_col = grid.keys().map(|&(_, c)| c).min().unwrap();
+    let max_col = grid.keys().map(|&(_, c)| c).max().unwrap();
+
+    for row in min_row..=max_row {
+        let line = (min_col..=max_col)
+            .map(|col| {
+                match grid.get(&(row, col)) {
+                    Some(&x) => x,
+                    None => Tile::Void,
+                }
+            })
+            .map(|t| t.into())
+            .collect::<String>();
+        println!("{}", line);
+    }
+}
+
+
 fn main() {
     let paths = read();
+    let grid = calculate_grid(&paths);
 
-    println!("paths: {:?}", paths);
+    display(&grid);
 }
