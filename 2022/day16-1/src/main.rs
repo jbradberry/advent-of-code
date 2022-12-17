2c594c0cfac5601ecf0525628076cdd92defb148
Jeff Bradberry <jeff.bradberry@gmail.com>
2022-12-17 00:21:56 -0500

    Kind of cheating, by slowly incrementing the number of permutations to do

--- 2022/day16-1/src/main.rs
+++ 2022/day16-1/src/main.rs
@@ -7,23 +7,6 @@ use itertools::Itertools;
 use regex::Regex;
 
 
-// fn read() -> HashMap<String, (u32, Vec<String>)> {
-//     let re = Regex::new(r"^Valve (\w+) has flow rate=(\d+); tunnel[s]? lead[s]? to valve[s]? ([\w, ]+)$").unwrap();
-//     let stdin = io::stdin();
-
-//     stdin.lock().lines()
-//         .map(|l| {
-//             let line = l.unwrap();
-//             let cap = re.captures(&line).unwrap();
-
-//             (cap.get(1).unwrap().as_str().to_string(),
-//              (cap.get(2).unwrap().as_str().parse::<u32>().unwrap(),
-//               cap.get(3).unwrap().as_str().split(", ").map(|s| s.to_string()).collect::<Vec<_>>()))
-//         })
-//         .collect()
-// }
-
-
 fn read() -> (HashMap<usize, (usize, Vec<usize>)>, usize) {
     let re = Regex::new(r"^Valve (\w+) has flow rate=(\d+); tunnel[s]? lead[s]? to valve[s]? ([\w, ]+)$").unwrap();
     let stdin = io::stdin();
@@ -55,15 +38,9 @@ fn read() -> (HashMap<usize, (usize, Vec<usize>)>, usize) {
 
 
 fn main() {
-    // let valves = read();
-
     let (valves, start) = read();
 
     // println!("valves: {:?}", valves);
-
-    // let mut cross = valves.iter()
-    //     .flat_map(|(k, (_, t))| t.iter().map(|v| ((k.to_string(), v.to_string()), 1)))
-    //     .collect::<HashMap<(String, String), usize>>();
 
     let mut cross = valves.iter()
         .flat_map(|(k, (_, t))| t.iter().map(|v| ((*k, *v), 1)))
@@ -72,16 +49,6 @@ fn main() {
     // println!("cross: {:?}", cross);
 
     loop {
-        // let new = cross.iter()
-        //     .permutations(2)
-        //     .filter(|x| (x[0].0.1 == x[1].0.0) && (x[0].0.0 != x[1].0.1))
-        //     .map(|x| (x[0].0.0.to_string(), x[1].0.1.to_string(), x[0].1 + x[1].1))
-        //     .filter(|(a, b, d)| {
-        //         let x = cross.get(&(a.to_string(), b.to_string()));
-        //         x.is_none() || d < x.unwrap()
-        //     })
-        //     .collect::<HashSet<_>>();
-
         let new = cross.iter()
             .permutations(2)
             .filter(|x| (x[0].0.1 == x[1].0.0) && (x[0].0.0 != x[1].0.1))
@@ -103,42 +70,36 @@ fn main() {
         }
     }
 
-    // println!("cross.len(): {}", cross.len());
-    // println!("cross:");
-    // for (k, v) in cross.iter() {
-    //     println!("{}: {:?}", v, k);
-    // }
-    // println!();
-
-    let useful = valves.iter()
+    let mut useful = valves.iter()
         .filter(|(_, v)| v.0 != 0)
-        .map(|(k, _)| *k)
-        .collect::<Vec<usize>>();
+        .map(|(k, v)| (*k, v.0))
+        .collect::<Vec<(usize, usize)>>();
+    useful.sort_by_key(|&(_, v)| v);
+    useful.reverse();
 
     println!("useful: {:?}", useful);
 
     let mut best = 0;
-    for p in useful.iter().permutations(useful.len()) {
-        // let mut location = "AA".to_string();
-
+    for p in useful.iter().permutations(8) {
         let mut location = start;
         let mut total = 0;
         let mut tick = 0;
 
         // println!("{:?}", p);
 
-        for v in &p {
-            // let d = cross.get(&(location.to_string(), v.to_string())).unwrap();
-
-            let d = cross.get(&(location, **v)).unwrap();
+        for (i, (v, r)) in p.iter().enumerate() {
+            let d = cross.get(&(location, *v)).unwrap();
 
             if *d < (30 - tick - 1) {
                 // println!("{} gets {} ticks", v, 30 - tick - d - 1);
-                total += (30 - tick - d - 1) * valves.get(&v).unwrap().0 as usize;
+                total += (30 - tick - d - 1) * r;
             }
             tick += d + 1;
             if tick >= 30 { break; }
-            location = **v;
+
+            // if there is no way we can catch up with what is left, skip the rest
+            if total < best && (best - total) > (30 - tick - 1) * (p[(i + 1)..].iter().map(|(_, x)| x).sum::<usize>()) { break; }
+            location = *v;
         }
 
         if total > best {
